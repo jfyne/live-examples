44bac0b4d18be8c8b25e49b0c9ffd07661a9be9d
Josh Fyne <josh.fyne@humanmademachine.com>
2021-02-11 11:22:51 +0000

    Update NPM example

---
+++ npm/README.md
@@ -0,0 +1,4 @@
+# NPM example
+
+Demonstrates importing the `@jfyne/live` package and compiling it with `esbuild`. This
+produces a `main.js` which will work in a browser.
--- npm/index.js
+++ npm/index.js
@@ -4,9 +4,11 @@ document.addEventListener("DOMContentLoaded", (_) => {
     const hooks = {
         "example-hook": {
             mounted: () => {
-                console.log("This is an example of passing hooks into live anywhere you want.");
-            }
-        }
+                console.log(
+                    "This is an example of passing hooks into live anywhere you want."
+                );
+            },
+        },
     };
     const l = new Live(hooks);
     l.init();
