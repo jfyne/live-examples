57934f4c519d685cda2db1e6894defde54ba4100
Josh Fyne <josh.fyne@gmail.com>
2023-01-03 17:08:39 +0000

    feat: Example of broadcasting at a server level.
    Refs:
     - https://github.com/jfyne/live/issues/61

--- chat/cmd/chat/main.go
+++ chat/cmd/chat/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/jfyne/live"
 	"github.com/jfyne/live-examples/chat"
@@ -9,7 +11,14 @@ import (
 
 func main() {
 	// Run the server.
-	http.Handle("/", live.NewHttpHandler(live.NewCookieStore("session-name", []byte("weak-secret")), chat.NewHandler()))
+	e := live.NewHttpHandler(live.NewCookieStore("session-name", []byte("weak-secret")), chat.NewHandler())
+	go func() {
+		for {
+			e.Broadcast("newmessage", chat.Message{ID: live.NewID(), User: "Room", Msg: fmt.Sprintf("The time is now %s", time.Now().Format(time.Kitchen))})
+			time.Sleep(1 * time.Minute)
+		}
+	}()
+	http.Handle("/", e)
 	http.Handle("/live.js", live.Javascript{})
 	http.Handle("/auto.js.map", live.JavascriptMap{})
 	http.ListenAndServe(":8080", nil)
